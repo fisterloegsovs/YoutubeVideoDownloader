e340d555c5c9770fea0e5792ad5215b662e8dcd3
fisterloegsovs <anders.persson@live.dk>
2022-12-26 19:57:15 +0100

    working program, can be extended

---
+++ yt_video_dl.py
@@ -0,0 +1,24 @@
+from pytube import YouTube as yt
+import argparse as ap
+
+
+
+def download(url, outPath):
+  ytLink = yt(url)
+  vidStream = ytLink.streams.get_highest_resolution()
+  vidStream.download(outPath)
+
+def main():
+  parser = ap.ArgumentParser()
+  parser.add_argument('url', help='URL of the YouTube video to download.')
+  parser.add_argument('--output', '-o', help='Output path for the video file.')
+  args = parser.parse_args()
+  outPath = args.output or '.'
+  downloadVideo = (args.url, outPath)
+
+if __name__ == '__main__':
+  main()
+
+
+
+
